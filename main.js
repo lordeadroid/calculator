615c9db17f15a27991bd4f235edead1cd21073fe
lordeadroid <rishabhrawat34@gmail.com>
2024-06-07 14:30:17 +0530

    taking only numbers as input

--- main.js
+++ main.js
@@ -1,5 +1,49 @@
+const ELEMENTS = {
+  DIV: "div",
+  IMG: "img",
+  MAIN: "main",
+  SPAN: "span",
+};
+
+const ATTRIBUTES = {
+  CLASS: "class",
+  SRC: "src",
+  ALT: "alt",
+};
+
+const createElement = (element) => document.createElement(element);
+const selectElement = (element) => document.querySelector(element);
+
+const createNumpad = () => {
+  const keys = new Array(10)
+    .fill(0)
+    .map((num, index) => num + index)
+    .reverse();
+};
+
 const main = () => {
-  console.log("JS is linked");
+  const container = createElement(ELEMENTS.DIV);
+  container.setAttribute(ATTRIBUTES.CLASS, "calculator");
+
+  const screen = createElement(ELEMENTS.DIV);
+  screen.setAttribute(ATTRIBUTES.CLASS, "screen");
+
+  const numpad = createElement(ELEMENTS.DIV);
+  numpad.setAttribute(ATTRIBUTES.CLASS, "numpad");
+
+  const body = selectElement("body");
+  body.appendChild(screen);
+
+  createNumpad();
+
+  onkeydown = (event) => {
+    const query = screen.innerText;
+
+    if (event.code.includes("Digit")) {
+      const newQuery = query + event.key;
+      screen.innerText = newQuery;
+    }
+  };
 };
 
 window.onload = main;
