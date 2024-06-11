cff641c0118df9ac7e07ef2273de4910372167f9
lordeadroid <rishabhrawat34@gmail.com>
2024-06-11 13:19:35 +0530

    added dropdown transition on the result

--- view.js
+++ view.js
@@ -11,7 +11,22 @@ class View {
     this.#input.innerText = input;
   }
 
+  #removeResultStyle() {
+    setTimeout(() => {
+      this.#result.style = "";
+    }, 1000);
+  }
+
+  #addStyleToResult() {
+    this.#result.style.position = "relative";
+    this.#result.style.top = "-2rem";
+    this.#result.style.transform = "translate(0, 2rem)";
+    this.#result.style.transition = "all 1000ms ease";
+  }
+
   render(result) {
     this.#result.innerText = result;
+    this.#addStyleToResult();
+    this.#removeResultStyle();
   }
 }
