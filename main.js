0adfc5fdeb6827b270254f331a11a7dbee8f1d7d
lordeadroid <rishabhrawat34@gmail.com>
2024-06-11 11:21:33 +0530

    created help section

--- main.js
+++ main.js
@@ -24,10 +24,33 @@ const getElements = () => {
   return { numbers, enterKey, clearKey, deleteKey, operators };
 };
 
+const createHelpSection = () => {
+  let clicked = false;
+
+  const calculator = selectElement(".calculator");
+  const instructionsDiv = selectElement(".instructions");
+
+  const helpSection = selectElement(".help-section");
+  helpSection.onclick = () => {
+    if (clicked === false) {
+      clicked = true;
+      instructionsDiv.classList.remove("hidden");
+      calculator.classList.add("blur");
+      return;
+    }
+
+    clicked = false;
+    instructionsDiv.classList.add("hidden");
+    calculator.classList.remove("blur");
+  };
+};
+
 const main = () => {
   const input = selectElement(".input");
   const result = selectElement(".result");
   const elements = getElements();
+
+  createHelpSection();
 
   const view = new View(input, result);
   const controller = new Controller(
